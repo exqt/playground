121141792928fd9e89fda5f4e20643d090cc986e
exqt <exqt_@naver.com>
2020-11-10 05:10:06 +0900

    add three.js

---
+++ src/scenes/threejsTest.js
@@ -0,0 +1,13 @@
+import Scene from "../scene"
+import * as THREE from "three.js"
+
+class Cube3dScene extends Scene {
+  constructor() {
+    super("cube3d", 400, 400)
+  }
+
+  update(dt) {
+  }
+}
+
+export default Cube3dScene
